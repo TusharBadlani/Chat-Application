f70cfb2798257d60bf33dd5602496a56b0064ea1
Tushar Badlani <tushar.badlani8@gmail.com>
2023-04-13 17:41:50 +0530

    Full Application made

---
+++ app/src/main/java/com/example/chatapplication/Activity/LoginActivity.kt
@@ -0,0 +1,59 @@
+package com.example.chatapplication.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chatapplication.R
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+        mAuth = FirebaseAuth.getInstance()
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LoginActivity, "User Doesnot Exist", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+    }
+}
